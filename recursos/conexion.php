72565cf10af2e7852e5fe409d8b61628ccd38153
dolzgonzalesrodrigo <rodrigogonzalesdolz@gmail.com>
2021-04-14 09:18:15 -0400

    cambio de base de datos

--- recursos/conexion.php
+++ recursos/conexion.php
@@ -1,6 +1,6 @@
 <?php 
 
-$conexion = mysqli_connect('localhost','root','','almacen2');
+$conexion = mysqli_connect('localhost','root','','base_arbell');
 
 if($conexion === false) { 
  echo 'Ha habido un error <br>'.mysqli_connect_error(); 
