3f78198e9f890c1e73229fbfb9f6ec27b7bc9dc9
cristianmc9000 <82479914+cristianmc9000@users.noreply.github.com>
2021-04-20 00:05:48 -0400

    utf8 conexion

--- recursos/conexion.php
+++ recursos/conexion.php
@@ -1,7 +1,7 @@
 <?php 
 
 $conexion = mysqli_connect('localhost','root','','base_arbell');
-
+$conexion->query("SET NAMES 'utf8'");
 if($conexion === false) { 
 echo 'Ha habido un error <br>'.mysqli_connect_error(); 
 }
