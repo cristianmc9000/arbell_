86eb6d280dbb7287c205f00b8148ad3b81588fe8
dolzgonzalesrodrigo <rodrigogonzalesdolz@gmail.com>
2021-05-03 20:13:15 -0400

    cantidadstock

--- recursos/ventas/buscar_producto.php
+++ recursos/ventas/buscar_producto.php
@@ -8,19 +8,20 @@ include('../conexion.php');
 if(isset($_GET["term"]))
 {
     //modificar consulta para que salga nombre de la linea ...
-    $result = $conexion->query("SELECT a.id, a.linea, a.descripcion, a.foto, a.pupesos, a.pubs, b.nombre FROM productos a, lineas b WHERE a.linea = b.codli AND id LIKE '%".$_GET["term"]."%' ORDER BY id ASC");
+    $result = $conexion->query("SELECT a.id, c.cantidad AS stock, a.linea, a.descripcion, a.foto, a.pupesos, a.pubs, b.nombre FROM productos a, lineas b, invcant c WHERE a.id = c.codp AND a.linea = b.codli AND id LIKE '%".$_GET["term"]."%' ORDER BY id ASC");
     $total_row = mysqli_num_rows($result); 
     $output = array();
     if($total_row > 0){
       foreach($result as $row)
       {
-       $temp_array = array();
-       $temp_array['id'] = $row['id'];
-       $temp_array['linea'] = $row['nombre'];
-       $temp_array['value'] = $row['descripcion'];
-       $temp_array['label'] = '<img class="zoom" src="'.$row['foto'].'" width="85" />   '.$row['descripcion'].'';
-       $temp_array['pupesos'] = $row['pupesos'];
-       $temp_array['pubs'] = $row['pubs'];
+        $temp_array = array();
+        $temp_array['id'] = $row['id'];
+        $temp_array['stock'] = $row['stock'];
+        $temp_array['linea'] = $row['nombre'];
+        $temp_array['value'] = $row['descripcion'];
+        $temp_array['label'] = '<img class="zoom" src="'.$row['foto'].'" width="85" />   '.$row['descripcion'].'';
+        $temp_array['pupesos'] = $row['pupesos'];
+        $temp_array['pubs'] = $row['pubs'];
        
        $output[] = $temp_array;
       }
--- templates/ventas/a_ventas.php
+++ templates/ventas/a_ventas.php
@@ -25,57 +25,49 @@
         <form id="insert_row" >
           <div class="input-field col s6">
               <div class="col s6">
-              <input type="text" id="search_data" placeholder="Buscar Lider/Experta" autocomplete="off" class="validate" required />
+              <input type="text" id="search_le" placeholder="Buscar Lider/Experta" autocomplete="off" class="validate" required />
               </div>
               <!-- codigo -->
               <div class="col s3">
-              <input type="number" id="ca" placeholder="código" autocomplete="off" required>
+              <input type="text" id="ca" placeholder="código" autocomplete="off" required>
               </div>
 
               <div class="col s2">
               <input id="descuento_" type="number" min="0" max="100" value="" class="validate" placeholder="% Descuento">
             </div>
-
           </div>
               <!-- boton insertar -->
               <!-- <div class="col s2">
               <button class="btn waves-effect waves-light btn-large" type="submit" ><i class="material-icons right">assignment</i>Insertar</button>
               </div> -->
         </form>
-        
       </div>
     </div>
 
 <!-- anadir buscar -->
-
-
-
     <div class="row">
-
     <div class="fuente" style="">
       <h5 align="">Buscar producto</h5>
       <div class="row">
         <form id="insert_row_producto" >
-          <div class="input-field col s4">
-            <div class="col s6">
+          <div class="input-field col s5">
+            <div class="col s5">
               <input type="text" id="search_producto" placeholder="Buscar producto" autocomplete="off" class="validate" required />
             </div>
-
+            <div class="col s1">
+              <b id="stock"></b>
+            </div>
             <div class="col s3">
               <input type="number" id="cantidad_" placeholder="Cantidad" autocomplete="off" required>
             </div>
-
             <div class="col s3">
               <input type="text" id="pubs_" placeholder="Precio en bs." required>
             </div>
-
-            
             <input type="text" id="id_" value="" hidden>
             <input type="text" id="linea_" value="" hidden>
             <input type="text" id="pupesos_" value="" hidden>
           <!--   <input type="text" id="pubs_" value="" hidden> -->
           </div>
-
           <div class="col s2">
             <button class="btn waves-effect waves-light btn-large" type="submit" ><i class="material-icons right">assignment</i>Insertar</button>
           </div>
@@ -130,7 +122,7 @@
 
 $(document).ready(function(){
     $('#modal').leanModal();
-    $('#search_data').autocomplete({
+    $('#search_le').autocomplete({
       source: "recursos/ventas/buscar_le.php",
       minLength: 1,
       select: function(event, ui)
@@ -139,7 +131,7 @@ $(document).ready(function(){
         if(ui.item.nivel == "experta"){
           $("#descuento_").val('30')
         }
-        $('#search_data').val(ui.item.value);  
+        $('#search_le').val(ui.item.value);  
       }
     }).data('ui-autocomplete')._renderItem = function(ul, item){
         // console.log(item)
@@ -155,11 +147,9 @@ $(document).ready(function(){
       minLength: 1,
       select: function(event, ui)
       {
-        $("#ca").val(ui.item.ca)
-        if(ui.item.nivel == "experta"){
-          $("#descuento_").val('30')
-        }
-        $('#search_data').val(ui.item.value);  
+        $("#pubs_").val(ui.item.pubs)
+        $("#stock").html(ui.item.stock)
+        $('#search_producto').val(ui.item.value);  
       }
     }).data('ui-autocomplete')._renderItem = function(ul, item){
         // console.log(item)
@@ -169,7 +159,6 @@ $(document).ready(function(){
         .appendTo(ul);
     }; 
 });
-
 /* --------------funcion insertar fila de producto---------------- */
 document.getElementById("insert_row_producto").addEventListener("submit", function (event) {
   event.preventDefault();
@@ -215,7 +204,7 @@ document.getElementById("insert_row_producto").addEventListener("submit", functi
   newRow.className = "_linea"
 
   newRow = newTableRow.insertCell(2)
-  newRow.textContent = $("#search_data").val()
+  newRow.textContent = $("#search_producto").val()
   newRow.className = "_descripcion"
 
   newRow = newTableRow.insertCell(3)
@@ -250,7 +239,7 @@ document.getElementById("insert_row_producto").addEventListener("submit", functi
   newRow.innerHTML = '<a href="#!" onclick="delete_row(event)" class="btn-floating red"><i class="material-icons">delete</i></a>'
 
 
-  $("#search_data").val("")
+  $("#search_producto").val("")
   $("#cantidad_").val("")
   $("#pupesos_").val("")
 });
