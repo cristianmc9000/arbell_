e19812ce18fc629b2d3756123d8ba0d4cbc6299c
dolzgonzalesrodrigo <rodrigogonzalesdolz@gmail.com>
2021-05-12 00:12:59 -0400

    modal_modificarusuario

--- templates/usuarios/a_usuarios.php
+++ templates/usuarios/a_usuarios.php
@@ -1,31 +1,25 @@
 <?php
 require('../../recursos/conexion.php');
-
 $_SESSION['filas'] = array(); 
 $Sql = "SELECT * FROM usuarios"; 
 $Busq = $conexion->query($Sql); 
 while($arr = $Busq->fetch_array()) 
     { 
-        $fila[] = array('ci'=>$arr['CI'], 'nombre'=>$arr['nombre'], 'apellidos'=>$arr['apellidos'], 'telefono'=>$arr['telefono']); 
+        $fila[] = array('ci'=>$arr['CI'], 'nombre'=>$arr['nombre'], 'apellidos'=>$arr['apellidos'], 'telefono'=>$arr['telefono'],'password'=>$arr['password']);
         array_push($_SESSION['filas'],$fila); 
     } 
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
-
 <style>
   .fuente{
   	font-family: 'Segoe UI light';
   	color: red;
   }
-
-
   table.highlight > tbody > tr:hover {
     background-color: #a0aaf0 !important;
   }
-
   #tabla1{
   	border-collapse: separate;
   	border-radius: 5px;
@@ -40,24 +34,19 @@ while($arr = $Busq->fetch_array())
     max-width: 230px;
     z-index: 10 !important;
     width: 230px;
-
   }
   .fijo:hover{
     background: #ffffff;
     margin-right:40px;
   }
 </style>
-
 </head>
 <body>
-
 <span class="fuente"><h3>Usuarios	
   <!-- Modal Trigger -->
   <a class="waves-effect waves-light btn-floating btn-large red" id="modal" href="#modal1"><i class="material-icons left">add</i></a></h3> 
 </span>
-
   <!-- TABLA -->
-
   <table id="tabla1" class="highlight">
     <thead>
       <tr>
@@ -69,7 +58,6 @@ while($arr = $Busq->fetch_array())
           <th>Borrar</th>
       </tr>
     </thead>
-
     <tbody>
     <?php foreach($fila as $a  => $valor){ ?>
       <tr>
@@ -77,15 +65,12 @@ while($arr = $Busq->fetch_array())
         <td><?php echo $valor["nombre"]." ".$valor["apellidos"] ?></td>
         <td><?php echo $valor["telefono"] ?></td>
         <td>Administrador</td>
-        <td><a href="#"><i class="material-icons">build</i></a></td>
+        <td><a href="#!" onclick="modificar_usuario('<?php echo $valor['ci']?>','<?php echo $valor['nombre']?>','<?php echo $valor['apellidos']?>','<?php echo $valor['telefono']?>')"><i class="material-icons">build</i></a></td>
         <td><a href="#"><i class="material-icons">delete</i></a></td>        
       </tr>
     <?php } ?>	
     </tbody>
   </table>
-
-
-
 <!-- MODAL DATOS -->
 <div class="row">
   <div id="modal1" class="modal col s4 offset-s4">
@@ -109,13 +94,13 @@ while($arr = $Busq->fetch_array())
                 <label for="nombre">Nombre:</label>
               </div>
               <div class="input-field col s6">
-                <input name="apellidos" type="text" value="">
+                <input name="apellidos" type="text" value="" autocomplete="off">
                 <label for="apellidos">Apellidos:</label>
               </div>
             </div>             
             <div class="row">
               <div class="input-field col s6">
-                <input name="password" type="password" value="">
+                <input name="password" type="password" value="" autocomplete="new-password">
                 <label for="password">Contraseña:</label>
               </div>
               <div class="input-field col s6">
@@ -123,21 +108,68 @@ while($arr = $Busq->fetch_array())
                   <label for="password1">Repita la contraseña:</label>
               </div>
             </div>
-
             <div class="modal-footer">
               <button class="btn waves-effect waves-light" type="submit" >Aceptar</button>
               <a href="#!" class=" modal-action modal-close waves-effect waves-green btn-flat">Cancelar</a>
             </div>
-
           </form>
         </div>
     </div>
-              
   </div>
 </div>
 
-
+<!-- -----------modal modificar usuario------------ -->
+<div class="row">
+  <div id="modal2" class="modal col s4 offset-s4">
+    <div class="modal-content">
+      <h4>Modificar usuario</h4>
+        <div class="row">
+          <form id="modificar_usuario" class="col s12">
+            <div class="row">
+              <div class="input-field col s6">
+                <input id="m_ci" name="ci" type="text" class="validate">
+                <label class="active" for="ci">Cédula de Identidad:</label>
+              </div>
+              <div class="input-field col s6">
+                <input id="m_telefono" name="telefono" type="number" class="validate">
+                <label class="active" for="telefono">Teléfono:</label>
+              </div>
+            </div>
+            <div class="row">
+              <div class="input-field col s6">
+                <input id="m_nombre" name="nombre" type="text" class="validate">
+                <label class="active" for="nombre">Nombre:</label>
+              </div>
+              <div class="input-field col s6">
+                <input id="m_apellidos" name="apellidos" type="text" value="" autocomplete="off">
+                <label class="active" for="apellidos">Apellidos:</label>
+              </div>
+            </div>             
+            <div class="row">
+              <div class="input-field col s6">
+                <input id="m_password" name="password" type="password" value="" 
+autocomplete="new-password">
+                <label for="password">Nueva Contraseña:</label>
+                <small class="helper-text" style="color: red">Dejar en blanco si no desea modificar</small>
+              </div>
+              <div class="input-field col s6">
+                  <input id="m_password1" name="password1" type="password">
+                  <label for="password1">Repita la contraseña:</label>
+              </div>
+            </div>
+            <div class="modal-footer">
+              <button class="btn waves-effect waves-light" type="submit" 
+>Aceptar</button>
+              <a href="#!" class=" modal-action modal-close waves-effect 
+waves-green btn-flat">Cancelar</a>
+            </div>
+          </form>
+        </div>
+    </div>
+  </div>
+</div>
 <div id="mensaje"></div>
+
 
 
 <!-- -----------------CRUD DE USUARIOS------------------------ -->
@@ -148,8 +180,6 @@ $(document).ready(function() {
 });
 var mensaje = $("#mensaje");
 mensaje.hide();
-
-
 /* agregar usuario */
 $("#agregar_usuario").on("submit", function(e){
     e.preventDefault();
@@ -166,13 +196,10 @@ $("#agregar_usuario").on("submit", function(e){
       if (echo !== "") {
         mensaje.html(echo);
         mensaje.show();
-
         $("#cuerpo").load("templates/usuarios/a_usuarios.php");
-
       }
     });
 });
-
 /* modificar usuario */
 $("#modificar_cliente").on("submit", function(e){
     e.preventDefault();
@@ -192,10 +219,8 @@ $("#modificar_cliente").on("submit", function(e){
       }
     });
 });
-
 /* borrar usuario */
 function borrar_cliente(id){
-
   document.getElementById("datos_borrar").innerHTML ='<input type="text" name="id" value="'+id+'" hidden/>';
   $('#modal4').openModal();
 }
@@ -217,9 +242,13 @@ $("#borrar_cliente").on("submit", function(e){
     });
 });
 
-
+function modificar_usuario(ci, nombre, apellidos, telefono){
+  $("#m_ci").val(ci);
+  $("#m_telefono").val(telefono);
+  $("#m_nombre").val(nombre);
+  $("#m_apellidos").val(apellidos);
+  $("#modal2").openModal()
+}
 </script>
-
-
 </body>
 </html>
