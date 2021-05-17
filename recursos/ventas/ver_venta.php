9de63838b00d691dadef988c37af49a2b5f82bd3
cristianmc9000 <82479914+cristianmc9000@users.noreply.github.com>
2021-05-17 16:10:41 -0400

    ver venta, consulta terminada

--- recursos/ventas/ver_venta.php
+++ recursos/ventas/ver_venta.php
@@ -1,10 +1,10 @@
 <?php
 require("../conexion.php"); 
 $codv=$_GET["codv"];
-$result= $conexion->query("SELECT a.codp, b.linea, b.descripcion, a.cantidad, a.pubs_cd FROM ");
+$result= $conexion->query("SELECT a.codv, a.codp, b.linea, b.descripcion, a.cantidad, a.pubs_cd FROM detalle_venta a, productos b WHERE a.codv = ".$codv);
 $row = $result->fetch_array(MYSQLI_ASSOC);
 
-echo $row;
+echo var_dump($row);
 
 
 ?>
