56c886772e020872a6e22364c57318431edfb8a4
dolzgonzalesrodrigo <rodrigogonzalesdolz@gmail.com>
2021-05-17 16:06:39 -0400

    espacio recursos

--- recursos/ventas/ver_venta.php
+++ recursos/ventas/ver_venta.php
@@ -4,8 +4,6 @@ $codv=$_GET["codv"];
 $result= $conexion->query("SELECT a.codp, b.linea, b.descripcion, a.cantidad, a.pubs_cd FROM ");
 $row = $result->fetch_array(MYSQLI_ASSOC);
 
-
-
 echo $row;
 
 
