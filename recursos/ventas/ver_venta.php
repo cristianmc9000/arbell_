c08b0fa074d01b92bc5a9ca05c762227ab533cc8
cristianmc9000 <82479914+cristianmc9000@users.noreply.github.com>
2021-05-18 19:26:25 -0400

    empezando pagos

--- recursos/ventas/ver_venta.php
+++ recursos/ventas/ver_venta.php
@@ -1,10 +1,14 @@
 <?php
 require("../conexion.php"); 
 $codv=$_GET["codv"];
-$result= $conexion->query("SELECT a.codv, a.codp, b.linea, b.descripcion, a.cantidad, a.pubs_cd FROM detalle_venta a, productos b WHERE a.codv = ".$codv);
-$row = $result->fetch_array(MYSQLI_ASSOC);
+$result= $conexion->query("SELECT a.codv, a.codp, (SELECT c.nombre FROM lineas c WHERE b.linea = c.codli) as linea, b.descripcion, a.cantidad, a.pubs, a.pubs_cd FROM detalle_venta a, productos b WHERE a.codp = b.id AND a.codv = ".$codv);
 
-echo var_dump($row);
+while($row = $result->fetch_array(MYSQLI_ASSOC)) {
+    $rows[] = $row;
+}
+
+// echo $rows[0]['codv']; //reading a single element of array
+echo json_encode($rows);
 
 
 ?>
--- templates/ventas/reg_ventas.php
+++ templates/ventas/reg_ventas.php
@@ -10,129 +10,231 @@ while($arr = $Busq->fetch_array())
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-<style>.fuente {
-    font-family: 'Segoe UI light';
-    color: red;
-}
-table.highlight>tbody>tr:hover {
-    background-color: #a0aaf0 !important;
-}
+    <style>
+    .fuente {
+        font-family: 'Segoe UI light';
+        color: red;
+    }
 
-.borde_tabla{
-    border: 1px solid;
-    border-collapse: collapse;
-}
-</style>
+    table.highlight>tbody>tr:hover {
+        background-color: #a0aaf0 !important;
+    }
 
+    .borde_tabla {
+        border: 1px solid;
+        border-collapse: collapse !important;
+    }
+
+    .borde_tabla tr td {
+        border: 1px solid;
+        border-collapse: collapse !important;
+    }
+
+    .borde_tabla tr th {
+        border: 1px solid;
+        border-collapse: collapse !important;
+    }
+    </style>
 </head>
+
 <body>
-<span class="fuente"><h3>Registro de ventas</h3>
-</span>
-
-<!-- TABLA -->
-<table id="tabla1" class="highlight">
-    <thead>
-        <tr>
-            <th>Código</th>
-            <th>Lider/Experta</th>
-            <th>Fecha de Venta</th>
-            <th>Monto Total</th>
-            <th>Tipo de Venta</th>
-            <th>Ver</th>
-            <th>Borrar</th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php foreach($fila as $a  => $valor){ ?>
-        <tr>
-            <td>
-                <?php echo $valor["codv"] ?>
-            </td>
-            <td>
-                <?php echo $valor["nombre"]." ".$valor["apellidos"] ?>
-            </td>
-            <td>
-                <?php echo $valor["fecha"]?>
-            </td>
-            <td>
-                <?php echo $valor["total"]?>
-            </td>
-            <td>
-                <?php if($valor["credito"] == "0"){echo "Contado";} else{echo "Crédito";} ?>
-            </td>
-            <td>
-                <a href="#!" onclick="ver_venta('<?php echo $valor['codv']?>')"><i class="material-icons">visibility</i></a> 
-                <!-- <a href="#!"><i class="material-icons">build</i></a> -->
-            </td>
-            <td>
-                <!-- <a href="#!" onclick="borrar_producto('<?php echo $valor['id'] ?>');"><i class="material-icons">delete</i></a> -->
-            </td>
-        </tr>
-        <?php } ?>
-    </tbody>
-</table>
-
-<!-- Modal registro de venta detalle de venta -->
-<div class="row">
-    <div id="modal1" class="modal">
-        <div class="modal-content">
-            <div class="row">
-                <div class="col s4">
-                    <span>Código arbell: </span><br>
-                    <span>Lider/Experta:</span>
+    <span class="fuente">
+        <h3>Registro de ventas</h3>
+    </span>
+    <!-- TABLA -->
+    <table id="tabla1" class="highlight">
+        <thead>
+            <tr>
+                <th>Código</th>
+                <th>Lider/Experta</th>
+                <th>Fecha de Venta</th>
+                <th>Monto Total</th>
+                <th>Tipo de Venta</th>
+                <th>Ver</th>
+                <!-- <th>Pagos</th> -->
+                <th>Borrar</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach($fila as $a  => $valor){ ?>
+            <tr class="hola">
+                <td>
+                    <?php echo $valor["codv"] ?>
+                </td>
+                <td>
+                    <?php echo $valor["nombre"]." ".$valor["apellidos"] ?>
+                </td>
+                <td>
+                    <?php echo $valor["fecha"]?>
+                </td>
+                <td>
+                    <?php echo $valor["total"]?>
+                </td>
+                <td style="text-align: center">
+                    <?php if($valor["credito"] == "0"){echo "Contado";} else{echo "<button onclick='pagos(event)'>Ver pagos</button>";} ?>
+                </td>
+                <td>
+                    <a href="#!" onclick="ver_venta('<?php echo $valor['codv']?>')"><i class="material-icons">visibility</i></a>
+                    <!-- <a href="#!"><i class="material-icons">build</i></a> -->
+                </td>
+                <td>
+                    <!-- <a href="#!" onclick="borrar_producto('<?php echo $valor['id'] ?>');"><i class="material-icons">delete</i></a> -->
+                </td>
+            </tr>
+            <?php } ?>
+        </tbody>
+    </table>
+    <!-- Modal registro de venta detalle de venta -->
+    <div class="row">
+        <div id="modal1" class="modal">
+            <div class="modal-content">
+                <div class="row">
+                    <div class="col s4">
+                        <span>Código arbell: </span><br>
+                        <span>Lider/Experta:</span>
+                    </div>
+                    <div class="col s4" style="text-align: center;">
+                        <span>Punto de venta: PRINCIPAL</span><br>
+                        <span>Forma de pago:</span><br>
+                        <span>Periodo:</span>
+                    </div>
+                    <div class="col s4" style="text-align:right">
+                        <span>Distribuidora: CARMIÑA</span>
+                    </div>
+                    <div class="col s12">
+                        <p>
+                            <h5>Items del comprobante</h5>
+                        </p>
+                    </div>
+                    <div class="col s12">
+                        <table id="detalle_ven" class="borde_tabla">
+                            <tr>
+                                <th>Código <br> (producto)</th>
+                                <th>Linea</th>
+                                <th>Descripción</th>
+                                <th>Cantidad</th>
+                                <th>P. unidad</th>
+                                <th>Subtotal</th>
+                            </tr>
+                            <tbody></tbody>
+                        </table>
+                    </div>
+                    <div class="col s4 offset-s8">
+                        <h5>TOTALES:</h5>
+                    </div>
+                    <div class="col s4 offset-s8">
+                        <table class="borde_tabla">
+                            <tr>
+                                <th>Items:</th>
+                                <td id="items"></td>
+                            </tr>
+                            <tr>
+                                <th>Ganancias experta:</th>
+                                <td id="gan_exp"></td>
+                            </tr>
+                            <tr>
+                                <th>Total a pagar:</th>
+                                <td id="total"></td>
+                            </tr>
+                        </table>
+                    </div>
                 </div>
-                <div class="col s4" style="text-align: center;">
-                    <span>Punto de venta: PRINCIPAL</span><br>
-                    <span>Forma de pago:</span><br>
-                    <span>Periodo:</span>
-                </div>
-                <div class="col s4" style="text-align:right">
-                    <span>Distribuidora: CARMIÑA</span>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col s12">
-                    <p><h5>Items del comprobante</h5></p>
-                </div>
-            <div class="col s12">
-                <table class="borde_tabla">
-                    <tr>
-                        <th>Código <br> (producto)</th>
-                        <th>Linea</th>
-                        <th>Descripción</th>
-                        <th>Cantidad</th>
-                        <th>P. unidad</th>
-                        <th>Subtotal</th>
-                    </tr>
-                </table>
-            </div>
             </div>
         </div>
     </div>
-</div>
-
-<div id="mensaje"></div>
+    <div id="mensaje"></div>
 
 <!-- -----------------CRUD DE USUARIOS------------------------ -->
 <script>
 $(document).ready(function() {
-    $('#tabla1').dataTable();
+    $('#tabla1').dataTable({
+        "order": [
+            [0, "desc"]
+        ]
+    });
     $('#modal').leanModal();
 });
 var mensaje = $("#mensaje");
 mensaje.hide();
 
 /* funcion ver venta */
-function ver_venta(codv){
-$.ajax({
-    url: "recursos/ventas/ver_venta.php?codv="+codv,
-    method: "GET",
-    success: function(response){
-        console.log(response);
-    }
-});
-    $("#modal1").openModal();
+function ver_venta(codv) {
+    detalle_venta(codv).then(respuesta => {
+        let total = 0
+        let cantidad = 0
+        let gan_exp = 0
+        var jsonParsedArray = JSON.parse(respuesta)
+        for (key in jsonParsedArray) {
+            if (jsonParsedArray.hasOwnProperty(key)) {
+                total += parseInt(jsonParsedArray[key]['cantidad']) * parseFloat(jsonParsedArray[key]['pubs_cd'])
+            }
+        }
+        total = total.toFixed(1)
+
+        //INSERTANDO FILAS A LA TABLA DETALLE DE VENTA 
+        let table = document.getElementById("detalle_ven")
+
+        $(".dinamic_rows").remove();
+        for (key in jsonParsedArray) {
+            if (jsonParsedArray.hasOwnProperty(key)) {
+                let newTableRow = table.insertRow(-1)
+                newTableRow.className = "dinamic_rows"
+                newRow = newTableRow.insertCell(0)
+                newRow.textContent = jsonParsedArray[key]['codp']
+
+                newRow = newTableRow.insertCell(1)
+                newRow.textContent = jsonParsedArray[key]['linea']
+
+                newRow = newTableRow.insertCell(2)
+                newRow.textContent = jsonParsedArray[key]['descripcion']
+
+                newRow = newTableRow.insertCell(3)
+                newRow.textContent = jsonParsedArray[key]['cantidad']
+
+                newRow = newTableRow.insertCell(4)
+                newRow.textContent = jsonParsedArray[key]['pubs_cd']
+
+                newRow = newTableRow.insertCell(5)
+                newRow.textContent = (parseInt(jsonParsedArray[key]['cantidad']) * parseFloat(jsonParsedArray[key]['pubs_cd'])).toFixed(1)
+
+                cantidad = cantidad + parseInt(jsonParsedArray[key]['cantidad'])
+                gan_exp = gan_exp + (parseFloat(jsonParsedArray[key]['pubs']) * parseInt(jsonParsedArray[key]['cantidad']) - parseFloat(jsonParsedArray[key]['pubs_cd']) * parseInt(jsonParsedArray[key]['cantidad']))
+
+            }
+        }
+
+        $("#total").html(total)
+        $("#items").html(cantidad)
+        $("#gan_exp").html((gan_exp).toFixed(1))
+        $("#modal1").openModal()
+    })
+}
+//OBTENER EL DETALLE DE VENTA EN JSON
+function detalle_venta(codv) {
+    return new Promise((resolve, reject) => {
+        $.ajax({
+            url: "recursos/ventas/ver_venta.php?codv=" + codv,
+            method: "GET",
+            success: function(response) {
+                resolve(response)
+            },
+            error: function(error) {
+                console.log(error)
+                reject(error)
+            }
+        })
+    })
+}
+
+function pagos(e) {
+   row = e.target.parentNode.parentNode
+   cell = row.getElementsByTagName("td")
+   console.log(cell[0].innerText)
+
+   
+
 }
 </script>
 
