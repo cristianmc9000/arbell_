d9e4413f675d3dd7b84b7e38798e79ce3554d3c8
cristianmc9000 <82479914+cristianmc9000@users.noreply.github.com>
2021-05-19 19:54:45 -0400

    cantidades correctas desde modificar inventario

--- recursos/inventarios/modificar_inventario.php
+++ recursos/inventarios/modificar_inventario.php
@@ -18,9 +18,6 @@ $cant = $cantidad - $cant_ant;
 
 // $year = $_SESSION['anio'];
 $periodo = $_SESSION["periodo"];
-	
-
-
 
 	//consulta modificar inventario 
 	$consulta ="UPDATE inventario SET pupesos='".$pupesos."', pubs='".$pubs."', cantidad='".$cantidad."', fecha_venc='".$fecha_v."' WHERE id= '".$id."' ";
@@ -37,15 +34,16 @@ $periodo = $_SESSION["periodo"];
 
 
 //PARA ACTUALIZAR A LAS CANTIDADES CORRECTAS SUMANDO CANTIDADES DESDE INVENTARIO
-// $consulta = "SELECT id FROM productos WHERE estado = 1";
-// $busq = $conexion->query($consulta);
+ $consulta = "SELECT id FROM productos WHERE estado = 1";
+ $busq = $conexion->query($consulta);
 
-// while ($arr = $busq->fetch_array()) {
-// 	$sql = "UPDATE `invcant` SET cantidad = (SELECT SUM(b.cantidad) FROM inventario b WHERE b.codp = '".$arr['id']."') WHERE codp = '".$arr['id']."'";
-// 	$res = $conexion->query($sql);
-// }
+ while ($arr = $busq->fetch_array()) {
+ 	//$sql = "UPDATE `invcant` SET cantidad = (SELECT SUM(b.cantidad) FROM inventario b WHERE b.codp = '".$arr['id']."') WHERE codp = '".$arr['id']."'";
+ 	$sql = "UPDATE invcant a SET a.cantidad = (SELECT IF((SELECT SUM(b.cantidad) FROM inventario b WHERE b.estado = 1 AND b.codp = '".$arr['id']."')>0, (SELECT SUM(b.cantidad) FROM inventario b WHERE b.estado = 1 AND b.codp = '".$arr['id']."'),0)) WHERE a.codp = '".$arr['id']."'";
+ 	$res = $conexion->query($sql);
+ }
 
-$res = $conexion->query("UPDATE invcant a SET a.cantidad = (SELECT SUM(b.cantidad) FROM inventario b WHERE b.estado = 1 AND b.codp = (SELECT c.codp FROM inventario c WHERE c.id = ".$id.")) WHERE a.codp = (SELECT d.codp FROM inventario d WHERE d.id = ".$id.")");
+//$res = $conexion->query("UPDATE invcant a SET a.cantidad = (SELECT SUM(b.cantidad) FROM inventario b WHERE b.estado = 1 AND b.codp = (SELECT c.codp FROM inventario c WHERE c.id = ".$id.")) WHERE a.codp = (SELECT d.codp FROM inventario d WHERE d.id = ".$id.")");
 
 if ($res) {
 	die('?mes='.$periodo);
