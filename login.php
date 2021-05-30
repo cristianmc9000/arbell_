c3338a5df1d411cf2c9559de6376eec632a5096c
Rodrigo Gonzales Dolz <52252456+rodrigogd1212@users.noreply.github.com>
2021-05-30 09:40:56 -0400

    formateado

--- login.php
+++ login.php
@@ -6,11 +6,11 @@ require('recursos/sesiones.php');
 ?>
 <!DOCTYPE html>
 <head>
-    <title>login</title>
-    <link rel="icon" type="image/x-icon" href="img/iconoarbell.ico" />
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="stylesheet" href="css/master.css">
+	<title>login</title>
+	<link rel="icon" type="image/x-icon" href="img/iconoarbell.ico" />
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<link rel="stylesheet" href="css/master.css">
 	<link rel="icon" href="img/iconoarbell.ico"></link>
 	<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
 	<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
@@ -22,98 +22,99 @@ require('recursos/sesiones.php');
 	<script src="js/datatable.js"></script>
 </head>
 <style>
-    .login-box{
-        background-color: transparent !important;
-    }
+	.login-box {
+		background-color: transparent !important;
+	}
 </style>
+
 <body>
-    <div class="login-box">
-        <img class="avatar" src="img/logoarbell.png" alt="Logo Arbell">
-        <h1>Ingresar al Sistema</h1>
-			<div class="formulario-acceso">
-				<form method="POST" id="acceso" action="" accept-charset="utf-8">
-					<!-- Usuario -->
-					<label for="username">Usuario</label>
-						<input style="color: white" type="text" placeholder="Ingresar Usuario" name="userAcceso" class="acceso validate" id="userAcceso" autocomplete="off" maxlength="20">
-					<!-- Contrasseña -->
-					<label for="password">Password</label>
-						<input style="color: white" type="password" placeholder="Ingresar Password" name="passAcceso" class="acceso" id="passAcceso"  autocomplete="off" maxlength="20">
-					<!-- boton ingresar -->
-						<input type="submit" name="acceso" value="Ingresar" style="cursor: pointer"> 
-				</form>
-			</div>
-    </div>
-<div id="mensaje"></div>
-<script>
-//Guardamos el controlador del div con ID mensaje en una variable
-var mensaje = $("#mensaje");
-//Ocultamos el contenedor
-mensaje.hide();
-//Cuando el formulario con ID acceso se envíe...
-$("#acceso").on("submit", function(e){
-	//Evitamos que se envíe por defecto
-	e.preventDefault();
-	//Creamos un FormData con los datos del mismo formulario
-	var formData = new FormData(document.getElementById("acceso"));
-	//Llamamos a la función AJAX de jQuery
-	$.ajax({
-		//Definimos la URL del archivo al cual vamos a enviar los datos
-		url: "recursos/acceder.php",
-		//Definimos el tipo de método de envío
-		type: "POST",
-		//Definimos el tipo de datos que vamos a enviar y recibir
-		dataType: "HTML",
-		//Definimos la información que vamos a enviar
-		data: formData,
-		//Deshabilitamos el caché
-		cache: false,
-		//No especificamos el contentType
-		contentType: false,
-		//No permitimos que los datos pasen como un objeto
-		processData: false
-	}).done(function(echo){
-		//Una vez que recibimos respuesta
-		//comprobamos si la respuesta no es vacía
-		if (echo !== "") {
-			
-			//Si hay respuesta (error), mostramos el mensaje
-			mensaje.html(echo);
-			mensaje.show();
-		} else {
-			//Si no hay respuesta, redirecionamos a donde sea necesario
-			//Si está vacío, recarga la página
-			window.location.replace("index.php");
-		}
-	});
-});
-//Cuando el formulario con ID acceso se envíe...
-$("#registro").on("submit", function(e){
-	//Evitamos que se envíe por defecto
-	e.preventDefault();
-	//Creamos un FormData con los datos del mismo formulario
-	var formData = new FormData(document.getElementById("registro"));
-	//Llamamos a la función AJAX de jQuery
-	$.ajax({
-		//Definimos la URL del archivo al cual vamos a enviar los datos
-		url: "recursos/registro.php",
-		//Definimos el tipo de método de envío
-		type: "POST",
-		//Definimos el tipo de datos que vamos a enviar y recibir
-		dataType: "HTML",
-		//Definimos la información que vamos a enviar
-		data: formData,
-		//Deshabilitamos el caché
-		cache: false,
-		//No especificamos el contentType
-		contentType: false,
-		//No permitimos que los datos pasen como un objeto
-		processData: false
-	}).done(function(echo){
-		//Cuando recibamos respuesta, la mostramos
-		mensaje.html(echo);
-		mensaje.show();
-	});
-});
-</script>
+	<div class="login-box">
+		<img class="avatar" src="img/logoarbell.png" alt="Logo Arbell">
+		<h1>Ingresar al Sistema</h1>
+		<div class="formulario-acceso">
+			<form method="POST" id="acceso" action="" accept-charset="utf-8">
+				<!-- Usuario -->
+				<label for="username">Usuario</label>
+				<input style="color: white" type="text" placeholder="Ingresar Usuario" name="userAcceso" class="acceso validate" id="userAcceso" autocomplete="off" maxlength="20">
+				<!-- Contrasseña -->
+				<label for="password">Password</label>
+				<input style="color: white" type="password" placeholder="Ingresar Password" name="passAcceso" class="acceso" id="passAcceso" autocomplete="off" maxlength="20">
+				<!-- boton ingresar -->
+				<input type="submit" name="acceso" value="Ingresar" style="cursor: pointer">
+			</form>
+		</div>
+	</div>
+	<div id="mensaje"></div>
+	<script>
+		//Guardamos el controlador del div con ID mensaje en una variable
+		var mensaje = $("#mensaje");
+		//Ocultamos el contenedor
+		mensaje.hide();
+		//Cuando el formulario con ID acceso se envíe...
+		$("#acceso").on("submit", function(e) {
+			//Evitamos que se envíe por defecto
+			e.preventDefault();
+			//Creamos un FormData con los datos del mismo formulario
+			var formData = new FormData(document.getElementById("acceso"));
+			//Llamamos a la función AJAX de jQuery
+			$.ajax({
+				//Definimos la URL del archivo al cual vamos a enviar los datos
+				url: "recursos/acceder.php",
+				//Definimos el tipo de método de envío
+				type: "POST",
+				//Definimos el tipo de datos que vamos a enviar y recibir
+				dataType: "HTML",
+				//Definimos la información que vamos a enviar
+				data: formData,
+				//Deshabilitamos el caché
+				cache: false,
+				//No especificamos el contentType
+				contentType: false,
+				//No permitimos que los datos pasen como un objeto
+				processData: false
+			}).done(function(echo) {
+				//Una vez que recibimos respuesta
+				//comprobamos si la respuesta no es vacía
+				if (echo !== "") {
+					//Si hay respuesta (error), mostramos el mensaje
+					mensaje.html(echo);
+					mensaje.show();
+				} else {
+					//Si no hay respuesta, redirecionamos a donde sea necesario
+					//Si está vacío, recarga la página
+					window.location.replace("index.php");
+				}
+			});
+		});
+		//Cuando el formulario con ID acceso se envíe...
+		$("#registro").on("submit", function(e) {
+			//Evitamos que se envíe por defecto
+			e.preventDefault();
+			//Creamos un FormData con los datos del mismo formulario
+			var formData = new FormData(document.getElementById("registro"));
+			//Llamamos a la función AJAX de jQuery
+			$.ajax({
+				//Definimos la URL del archivo al cual vamos a enviar los datos
+				url: "recursos/registro.php",
+				//Definimos el tipo de método de envío
+				type: "POST",
+				//Definimos el tipo de datos que vamos a enviar y recibir
+				dataType: "HTML",
+				//Definimos la información que vamos a enviar
+				data: formData,
+				//Deshabilitamos el caché
+				cache: false,
+				//No especificamos el contentType
+				contentType: false,
+				//No permitimos que los datos pasen como un objeto
+				processData: false
+			}).done(function(echo) {
+				//Cuando recibamos respuesta, la mostramos
+				mensaje.html(echo);
+				mensaje.show();
+			});
+		});
+	</script>
 </body>
+
 </html>
