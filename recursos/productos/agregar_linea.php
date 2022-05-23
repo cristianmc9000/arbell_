b3b6fb591efe3fe6870ed370055e36724b544f8a
cristianmc9000 <cristian.preof1@gmail.com>
2022-05-23 09:41:24 -0400

    conexion

--- recursos/productos/agregar_linea.php
+++ recursos/productos/agregar_linea.php
@@ -4,7 +4,7 @@ require('../sesiones.php');
 session_start();
 $periodo = $_SESSION["periodo"];
 $nombre = $_POST["linea_"];
-
+// UPDATE productos a SET foto = CONCAT("images/fotos_prod/",id,".jpg") PARA COPIAR DATOS DE UNA CELDA A OTRA concatenando
 $sql = $conexion->query("INSERT INTO  lineas (nombre, periodo) VALUES ('".$nombre."', ".$periodo.")");
 
 echo "?mes=".$periodo;
