86f378a6036211f2a3d8a920de91bea7b246280b
cristianmc9000 <cristian.preof1@gmail.com>
2022-05-30 15:53:13 -0400

    correciones

--- recursos/productos/borrarproducto.php
+++ recursos/productos/borrarproducto.php
@@ -8,8 +8,9 @@ $idPOST = $_POST["id"];
 $periodo = $_SESSION["periodo"];
 $year = $_SESSION['anio'];
 
-	$res = $conexion->query("SELECT cantidad FROM invcant WHERE codp = ".$idPOST);
+	$res = $conexion->query("SELECT cantidad FROM invcant WHERE codp = '".$idPOST."'");
 	$res = $res->fetch_all();
+
 	if ($res[0][0] < 1) {
 		$consultaBorrar = "UPDATE productos SET estado=0 WHERE id= '".$idPOST."'";
 		if(mysqli_query($conexion, $consultaBorrar) or die(mysqli_error($conexion))){
--- recursos/ventas/buscar_producto.php
+++ recursos/ventas/buscar_producto.php
@@ -6,7 +6,7 @@ include('../conexion.php');
 if(isset($_GET["term"]))
 {
     //modificar consulta para que salga nombre de la linea ...
-    $result = $conexion->query("SELECT a.id, a.periodo, (SELECT d.pupesos FROM inventario d WHERE d.fecha_reg = (SELECT MAX(e.fecha_reg) FROM inventario e WHERE e.codp = a.id) AND d.codp = a.id AND CONCAT(d.codp,' ',a.descripcion) LIKE '%".$_GET["term"]."%' LIMIT 1) AS maxpupesos,c.cantidad AS stock, a.linea, a.descripcion, a.foto, b.nombre FROM productos a, lineas b, invcant c WHERE a.id = c.codp AND a.linea = b.codli AND CONCAT(a.id,' ',a.descripcion) LIKE '%".$_GET["term"]."%' ORDER BY a.id ASC");
+    $result = $conexion->query("SELECT a.id, a.periodo, (SELECT d.pupesos FROM inventario d WHERE d.fecha_reg = (SELECT MAX(e.fecha_reg) FROM inventario e WHERE e.codp = a.id) AND d.estado = 1 AND d.codp = a.id AND CONCAT(d.codp,' ',a.descripcion) LIKE '%".$_GET["term"]."%' LIMIT 1) AS maxpupesos,c.cantidad AS stock, a.linea, a.descripcion, a.foto, b.nombre FROM productos a, lineas b, invcant c WHERE a.id = c.codp AND a.linea = b.codli AND CONCAT(a.id,' ',a.descripcion) LIKE '%".$_GET["term"]."%' ORDER BY a.id ASC");
 
     $total_row = mysqli_num_rows($result); 
     $output = array();
